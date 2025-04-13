fa4c7d5ac6a2abdafdec5a61c2d61b88cbbd2ee5
BosenkoSergey <47383173+BosenkoSergey@users.noreply.github.com>
2025-04-13 11:30:17 +0300

    Delete SimRegisPortal.Mailing directory

--- SimRegisPortal.Mailing/Models/Common/RecipientDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SimRegisPortal.Mailing.Models.Common
-{
-    public record RecipientDto
-    {
-        public string Email { get; init; } = null!;
-        public string Name { get; init; } = null!;
-    }
-}
