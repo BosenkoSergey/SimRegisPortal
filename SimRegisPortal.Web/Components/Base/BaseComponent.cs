7f125ce88fba30469436178784b55c0489590c3b
Serhii Bosenko <BosenkoSergey@gmail.com>
2025-04-21 16:41:33 +0300

    add OnFirstInitializedAsync

--- SimRegisPortal.Web/Components/Base/BaseComponent.cs
+++ SimRegisPortal.Web/Components/Base/BaseComponent.cs
@@ -8,4 +8,27 @@ public abstract class BaseComponent : ComponentBase
 {
     [Inject] protected IUiNotifier Notifier { get; set; } = default!;
     [Inject] protected ISender Mediator { get; set; } = default!;
+
+    private bool _initialized;
+
+    protected sealed override async Task OnInitializedAsync()
+    {
+        if (_initialized)
+        {
+            return;
+        }
+
+        _initialized = true;
+
+        try
+        {
+            await OnFirstInitializedAsync();
+        }
+        catch (Exception ex)
+        {
+            await Notifier.Exception(ex);
+        }
+    }
+
+    protected virtual Task OnFirstInitializedAsync() => Task.CompletedTask;
 }
