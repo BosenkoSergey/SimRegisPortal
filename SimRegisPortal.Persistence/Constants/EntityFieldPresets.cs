0938f239405213c14b33ff2fae3d7ac9d340175e
Serhii Bosenko <BosenkoSergey@gmail.com>
2025-04-27 09:54:47 +0300

    fix ExchangeRate precision

--- SimRegisPortal.Persistence/Constants/EntityFieldPresets.cs
+++ SimRegisPortal.Persistence/Constants/EntityFieldPresets.cs
@@ -6,7 +6,7 @@ public class EntityFieldPresets
     public const string DateTimeType = "datetime2";
     public const string GuidType = "uniqueidentifier";
     public const string MoneyType = "decimal(18,2)";
-    public const string RateType = "decimal(18,10)";
+    public const string RateType = "decimal(18,7)";
     public const string HoursType = "decimal(5,2)";
 
     public const string DefaultGuid = "NEWSEQUENTIALID()";
---
+++ SimRegisPortal.Persistence/Migrations/20250427065430_FixExchangeRatePrecision.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimRegisPortal.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixExchangeRatePrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "ExchangeRate",
+                type: "decimal(18,7)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,10)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "ExchangeRate",
+                type: "decimal(18,10)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,7)");
+        }
+    }
+}
