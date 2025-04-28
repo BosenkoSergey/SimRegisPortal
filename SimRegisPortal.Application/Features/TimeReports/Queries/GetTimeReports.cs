9485635ca29d79233aa15ec56a04d7c0dc527cc5
Serhii Bosenko <BosenkoSergey@gmail.com>
2025-04-28 18:17:19 +0300

    add GetTimeReportsQuery

---
+++ SimRegisPortal.Application/Features/TimeReports/Queries/GetTimeReports.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using SimRegisPortal.Application.Features.Base.Queries;
+using SimRegisPortal.Application.Models.Entities;
+using SimRegisPortal.Core.Entities;
+using SimRegisPortal.Persistence.Context;
+
+namespace SimRegisPortal.Application.Features.TimeReports.Queries;
+
+public sealed record GetTimeReportsQuery
+    : GetManyQuery<TimeReportDto>;
+
+internal sealed class GetTimeReportsHandler(AppDbContext dbContext, IMapper mapper)
+    : GetManyHandler<GetTimeReportsQuery, TimeReport, TimeReportDto>(dbContext, mapper)
+{
+    protected override IEnumerable<TimeReportDto> CreateResponse(IEnumerable<TimeReport> entities)
+    {
+        var response = base.CreateResponse(entities);
+        var timeReportIds = response.Select(r => r.Id).ToList();
+
+        var hoursByTimeReportId = Repository
+            .SelectMany(r => r.Activities)
+            .Where(a => timeReportIds.Contains(a.TimeReportId))
+            .GroupBy(a => a.TimeReportId)
+            .Select(g => new
+            {
+                TimeReportId = g.Key,
+                TotalHours = g.Sum(a => a.Hours)
+            })
+            .ToDictionary(x => x.TimeReportId, x => x.TotalHours);
+
+        foreach (var report in response)
+        {
+            report.Hours = hoursByTimeReportId.GetValueOrDefault(report.Id, 0);
+        }
+
+        return response;
+    }
+}
---
+++ SimRegisPortal.Application/Models/Entities/TimeReportDto.cs
@@ -0,0 +1,14 @@
+﻿using SimRegisPortal.Core.Enums;
+
+namespace SimRegisPortal.Application.Models.Entities;
+
+public sealed class TimeReportDto : BaseEntityDto<Guid>
+{
+    public Guid EmployeeId { get; set; }
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public TimeReportStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public decimal Hours { get; set; }
+}
