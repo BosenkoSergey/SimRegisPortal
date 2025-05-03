0a7c896fb2b48db11c9095820e0e8478810d76e5
Serhii Bosenko <BosenkoSergey@gmail.com>
2025-05-03 17:29:25 +0300

    create BaseEditPage

---
+++ SimRegisPortal.Web/Components/Base/BaseEditPage.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SimRegisPortal.Application.Models.Entities;
+
+namespace SimRegisPortal.Web.Components.Base;
+
+public abstract class BaseEditPage<TKey, TDto> : BaseComponent
+    where TKey : struct
+    where TDto : BaseEntityDto<TKey>, new()
+{
+    [Parameter] public TKey? Id { get; set; }
+    [Parameter] public string PageTitle { get; set; } = "";
+
+    protected MudForm? _form;
+    protected TDto? _model;
+
+    protected string PageTitleText => $"{(_model?.IsNew is true ? "Add" : "Edit")} {PageTitle}";
+
+    protected abstract IRequest<TDto> GetCommand(TKey Id);
+    protected abstract IRequest<TDto> SaveCommand(TDto model);
+
+    protected virtual TDto CreateNew()
+    {
+        return new TDto() { IsNew = true };
+    }
+
+    protected async Task LoadModelAsync()
+    {
+        if (Id.HasValue)
+        {
+            var result = await SendSafeAsync(GetCommand(Id.Value));
+            if (result.IsSuccess)
+            {
+                _model = result.Value;
+            }
+        }
+        else
+        {
+            _model = CreateNew();
+        }
+    }
+
+    protected async Task Save()
+    {
+        if (!await ValidateForm(_form)) return;
+        if (_model is null) return;
+
+        var result = await SendSafeAsync(SaveCommand(_model));
+        if (result.IsSuccess)
+        {
+            _model = result.Value;
+            await Notifier.Success("Saved successfully.");
+        }
+    }
+}
