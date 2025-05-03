6195beb1e1175b97fd0ce71582af3a17e254a365
Serhii Bosenko <BosenkoSergey@gmail.com>
2025-05-03 16:09:07 +0300

    add Employees page

--- SimRegisPortal.Core/Enums/SalaryScheme.cs
+++ SimRegisPortal.Core/Enums/SalaryScheme.cs
@@ -1,8 +1,15 @@
-﻿namespace SimRegisPortal.Core.Enums;
+﻿using System.ComponentModel;
+
+namespace SimRegisPortal.Core.Enums;
 
 public enum SalaryScheme
 {
+    [Description("FOP 2")]
     FOP2 = 1,
+
+    [Description("FOP 3")]
     FOP3 = 2,
+
+    [Description("GIG Contract")]
     GIG = 3
 }
