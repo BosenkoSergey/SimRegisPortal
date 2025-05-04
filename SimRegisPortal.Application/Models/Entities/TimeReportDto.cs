967e129b59a89f542b04f48d3571ed778fba9e8c
Serhii Bosenko <BosenkoSergey@gmail.com>
2025-05-04 15:05:15 +0300

    add TimeReports page

--- SimRegisPortal.Application/Models/Entities/TimeReportDto.cs
+++ SimRegisPortal.Application/Models/Entities/TimeReportDto.cs
@@ -6,7 +6,7 @@ public sealed class TimeReportDto : BaseEntityDto<Guid>
 {
     public Guid EmployeeId { get; set; }
     public int Year { get; set; }
-    public int Month { get; set; }
+    public Month Month { get; set; }
     public TimeReportStatus Status { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
